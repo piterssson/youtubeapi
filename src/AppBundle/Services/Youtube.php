6ae0b8493db60f4116265f33f02d72e8af3755d6
piotrswidurski <piotr.swidurski@gmail.com>
2017-11-28 15:35:24 +0100

    add services

--- src/AppBundle/Services/Youtube.php
+++ src/AppBundle/Services/Youtube.php
@@ -3,13 +3,31 @@
 namespace AppBundle\Services;
 
 use \GuzzleHttp\Client;
+use Doctrine\ORM\EntityManager;
+use AppBundle\Entity\Videos;
 
 class Youtube
 {
+    private $videoRepository;
+    private $em;
+
+    public function __construct($videoRepository, EntityManager $em)
+    {
+        $this->videoRepository = $videoRepository;
+        $this->em = $em;
+    }
+
     public function getapi()
     {
         $client = new Client();
         $res = $client->request('GET', 'https://www.googleapis.com/youtube/v3/search?part=id&channelId=UCrFx0KTUzpDQY2mkLWoIdyg&publishedAfter=2017-10-02T00%3A00%3A00Z&key=AIzaSyBR7D0zD3jh9RTiQuXrGapwSwFdmlnuT5w');
-        var_dump(json_decode($res->getBody()));
+        $body = json_decode($res->getBody());
+        foreach ($body->items as $item) {
+            $result = $this->videoRepository->findOneByVideoid($item->id->videoId);
+            if ($result == NULL) {
+                $videos = new Videos();
+                echo 'null<br>';
+            }
+        }
     }
 }
