ecafcd52f2d88151e6177ef881c66cc2c20650c8
piotrswidurski <piotr.swidurski@gmail.com>
2017-11-28 16:09:46 +0100

    fix controller

--- src/AppBundle/Services/Youtube.php
+++ src/AppBundle/Services/Youtube.php
@@ -5,7 +5,6 @@ namespace AppBundle\Services;
 use \GuzzleHttp\Client;
 use Doctrine\ORM\EntityManager;
 use AppBundle\Entity\Videos;
-use Symfony\Component\Validator\Constraints\DateTime;
 
 class Youtube
 {
@@ -20,8 +19,18 @@ class Youtube
 
     public function getNew()
     {
+        $date = date('Y-m-d');
+        $time = date('H:i:s', strtotime('-2 hour'));
+        $fulldate = $date.'T'.$time.'Z';
         $client = new Client();
-        $res = $client->request('GET', 'https://www.googleapis.com/youtube/v3/search?part=id&channelId=UCrFx0KTUzpDQY2mkLWoIdyg&publishedAfter=2017-10-02T00%3A00%3A00Z&key=AIzaSyBR7D0zD3jh9RTiQuXrGapwSwFdmlnuT5w');
+        $res = $client->request('GET', 'https://www.googleapis.com/youtube/v3/search', [
+            'query' => [
+                'part'           => 'id',
+                'channelId'      => 'UCrFx0KTUzpDQY2mkLWoIdyg',
+                'publishedAfter' => $fulldate,
+                'key'            => 'AIzaSyBR7D0zD3jh9RTiQuXrGapwSwFdmlnuT5w'
+            ]
+        ]);
         $body = json_decode($res->getBody());
         foreach ($body->items as $item) {
             $result = $this->videoRepository->findOneByVideoid($item->id->videoId);
